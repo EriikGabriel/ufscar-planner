181a702c3c42b215d995a9a7f86b34839ab659e7
EriikGabriel <erikgabriel.lins@hotmail.com>
2024-08-09 03:46:30 -0300

    feat: add prerequisites in mandatory disciplines

--- app/components/ui/data-table-columns.tsx
+++ app/components/ui/data-table-columns.tsx
@@ -97,6 +97,14 @@ export const mandatoryColumns: ColumnDef<Tables<"disciplines">>[] = [
     header: "Ações",
     cell: ({ row }) => (
       <div className="flex gap-3">
+        <PrerequisitesDialog
+          discipline={row.getValue("name")}
+          prerequisites={row.getValue("prerequisites")}
+        >
+          <Button variant="ghost" size="icon" className="hover:text-green-400">
+            <Network className="h-4 w-4" />
+          </Button>
+        </PrerequisitesDialog>
         <DisciplineSheet
           disciplineType="mandatory"
           discipline={{
